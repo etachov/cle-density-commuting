9a5eaf356d5c71cbcf97edf60680b05a0b7c0368
Evan <evan.tachovsky@gmail.com>
2015-05-19 21:31:11 -0400

    Initial data and code commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# cle-density-commuting
+# Cleveland Density and Commuting Charts
+
+These charts were created for this <a href="http://beltmag.com/why-cant-cleveland-be-more-like-a-bicycle/" target="_blank">article</a> in Belt Magazine on urban density and commuting.
+
+The density_commuting_clean.R script downloads and cleans the data for the chart from the U.S. Census and Wikipedia, making heavy use of the lovely <a href="http://blog.rstudio.org/2014/01/17/introducing-dplyr/" target="_blank">dplyr</a>. And density_commuting_charts.R creates the final charts using <a href="http://ggplot2.org/" target="_blank>ggplot2</a> Hadleyverse 4eva.
+
+![](https://github.com/etachov/cle-density-commuting/blob/master/Density Rank.png)
+
+![](https://github.com/etachov/cle-density-commuting/blob/master/Biking v. Walking.png)
---
+++ density_commuting_clean.R
@@ -0,0 +1,55 @@
+library(readxl)
+library(dplyr)
+library(rvest) 
+
+# download bike data from census.gov and import with read_excel
+download.file("http://www.census.gov/hhes/commuting/files/2014/Supplemental-Table3.xlsx", destfile = "bike.xlsx")
+
+bike_raw <- read_excel("bike.xlsx", skip = 5)
+
+# renaming columns to make life easier
+colnames(bike_raw) <- c("city.state", "num.bike", "per.bike", "margin.bike")
+
+# download waking data from census.gov and import with read_excel
+download.file("http://www.census.gov/hhes/commuting/files/2014/Supplemental-Table6.xlsx", destfile = "walk.xlsx")
+
+walk_raw <- read_excel("walk.xlsx", skip = 5)
+
+colnames(walk_raw) <- c("city.state", "num.walk", "per.walk", "margin.walk")
+
+# merge our merge the biking and walking data 
+comb_raw <- left_join(bike_raw, walk_raw)
+
+# clean up the city.state names so we can merge with population data
+comb_clean <- comb_raw %>%
+  filter(!is.na(per.walk)) %>%
+  mutate(city.state = gsub(" city", "", city.state), # don't need city lable
+         city.state = gsub(" municipality", "", city.state), # or municipality
+         city.state = gsub(" \\(balance\\)", "", city.state), # or (balance)
+         city.state = gsub("-Davidson metropolitan government", ", Tennessee", city.state), # then fixing a very specific problem with TN
+         per.bike.walk = per.bike + per.walk) 
+
+# download population data from wikipedia (this checks out with the census site and is already formatted in a nice table)
+pop_raw <- html("https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population") %>%
+  html_node("table.wikitable") %>%
+  html_table()
+
+colnames(pop_raw) <- c("rank", "city", "state", "pop.2013", "pop.2010", "change", "area.sqm", "density", "location")
+
+# clean up the random symbols and other anomalies
+pop_clean <- pop_raw %>% 
+  select(-location) %>%
+  mutate(rank = as.numeric(gsub("-\\(T\\)", "", rank)),
+         change = as.numeric(gsub("−", "-", gsub(".*♠|%|+", "", change))), # regexp means (.) any character, (*) any number of times until the symbols
+         pop.2013 = as.numeric(gsub(",", "", pop.2013)),
+         pop.2010 = as.numeric(gsub(",", "", pop.2010)),
+         city = gsub("\\d|\\[|\\]", "", city), # removing back brackets and digits with regular expression
+         area.sqm = as.numeric(gsub(".*♠|sq.*$", "", area.sqm)),
+         density = as.numeric(gsub(",|.*♠|per.*$", "", density)),    
+         city.state = paste(city, state, sep = ", ")) %>%
+  filter(rank < 51)
+
+# merge population data and commuting data
+fin_clean <- left_join(pop_clean, comb_clean)
+
+write.csv(fin_clean, "cle_density_commuting.csv", row.names = F)
